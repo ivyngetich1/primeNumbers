aad0b1a7a626c4849436ccc95849d18f4f24fb03
ivyngetich1 <ivyngetich1@gmail.com>
2016-10-08 08:41:32 +0300

    Add files via upload

---
+++ pNumbers Ivy.py
@@ -0,0 +1,10 @@
+def primeNumber(n):
+    for i in range (2,n+1):
+        count = 0
+        for j in range (2,i):
+            if i % j != 0:
+                count +=1
+                if count == i-2:
+                    print (i)
+   
+
